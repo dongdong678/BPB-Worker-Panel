f59b30641f0752c50a3a233f060012c891a93c39
dongdong <43347340+dongdong678@users.noreply.github.com>
2024-09-01 19:59:24 +0800

    Update _worker.js
    更新UUID

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
 // let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
-let userID = 'b5b9bf10-bd99-4aed-863f-e9b55ac55e42';
+let userID = 'cf29ca88-85be-4f42-b00b-7c54b7c717d1';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
